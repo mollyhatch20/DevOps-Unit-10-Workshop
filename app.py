2964af0eaf92714c0ecb068160486edccb7e39cd
“mollyhatch20” <mahatch97@yahoo.com>
2026-04-07 15:30:11 +0100

    update app.py and requirements

--- app.py
+++ app.py
@@ -1,82 +1,38 @@
-from flask import Flask, render_template, request
-from datetime import datetime, timezone
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License in the project root for
+# license information.
+# --------------------------------------------------------------------------
 
-from werkzeug.utils import redirect
-from flask_config import Config
-from data.database import initialise_database, add_order, clear_orders, count_orders, get_orders_to_display, get_queued_count, get_recently_placed_count, get_recently_processed_count
-from scheduled_jobs import initialise_scheduled_jobs
-from products import create_product_download
-import requests
-import logging
+from azure.monitor.opentelemetry import configure_azure_monitor
 
-logging.basicConfig(level=logging.INFO)
+# Configure Azure monitor collection telemetry pipeline
+configure_azure_monitor()
 
-app = Flask(__name__)
-app.config.from_object(Config)
+# Import Flask after running configure_azure_monitor()
+import flask  # pylint: disable=wrong-import-position, wrong-import-order
 
-initialise_database(app)
-initialise_scheduled_jobs(app)
+app = flask.Flask(__name__)
 
 
+# Requests sent to the flask application will be automatically captured
 @app.route("/")
-def index():
-    orders = get_orders_to_display()
-    queue_count = get_queued_count()
-    recently_placed_count = get_recently_placed_count()
-    recently_processed_count = get_recently_processed_count()
-    scenarios = [
-        { 'display': 'Add broken order', 'value': 'BrokenOrder' },
-        { 'display': 'Monitoring Load', 'value': 'HighLoad' },
-        { 'display': 'Queue Reliability', 'value': 'UnreliableProcessing' },
-        { 'display': 'System Monitoring', 'value': 'VeryHighLoad' },
-        { 'display': 'Reset to initial', 'value': 'Reset' }
-    ]
-
-    return render_template(
-        "layout.html", orders=orders, queue_count=queue_count, recently_placed_count=recently_placed_count,
-        recently_processed_count=recently_processed_count, scenarios=scenarios
-    )
-
-@app.route("/count")
-def count():
-    return { 'count': count_orders() }
+def test():
+    return "Test flask request"
 
 
-@app.route("/new", methods=["POST"])
-def new_order():
-    product = request.json["product"]
-    customer = request.json["customer"]
-    date_placed = request.json["date_placed"] or datetime.now(tz=timezone.utc)
-    download = create_product_download(product)
-    try:
-        order = add_order(product, customer, date_placed, None, download)
-    except Exception as e:
-        return str(e)
-
-    return f"Added: {order}"
+# Exceptions that are raised within the request are automatically captured
+@app.route("/exception")
+def exception():
+    raise Exception("Hit an exception")  # pylint: disable=broad-exception-raised
 
 
-@app.route("/scenario", methods=["POST"])
-def set_scenario():
-    scenario = request.form["scenario"]
+# Requests sent to this endpoint will not be tracked due to
+# flask_config configuration
+@app.route("/ignore")
+def ignore():
+    return "Request received but not tracked."
 
-    if scenario == 'BrokenOrder':
-        product = 'Product from the future'
-        download = create_product_download(product)
-        add_order('Product from the future', 'Me', '3000-01-01T12:00:00Z', None, download)
-        return redirect('/')
-
-    if scenario == 'Reset':
-        clear_orders()
-
-    response = requests.post(
-        app.config["FINANCE_PACKAGE_URL"] + "/scenario",
-        json=scenario
-    )
-
-    response.raise_for_status()
-
-    return redirect('/')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="localhost", port=8080)
