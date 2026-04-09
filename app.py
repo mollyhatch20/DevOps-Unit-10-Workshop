ee324a93294845435dd96311552d6c62b34c560e
“mollyhatch20” <mahatch97@yahoo.com>
2026-04-09 14:16:29 +0100

    update indents

--- app.py
+++ app.py
@@ -20,63 +20,62 @@ initialise_scheduled_jobs(app)
 
 @app.route("/")
 def index():
-orders = get_orders_to_display()
-queue_count = get_queued_count()
-recently_placed_count = get_recently_placed_count()
-recently_processed_count = get_recently_processed_count()
-scenarios = [
-{ 'display': 'Add broken order', 'value': 'BrokenOrder' },
-{ 'display': 'Monitoring Load', 'value': 'HighLoad' },
-{ 'display': 'Queue Reliability', 'value': 'UnreliableProcessing' },
-{ 'display': 'System Monitoring', 'value': 'VeryHighLoad' },
-{ 'display': 'Reset to initial', 'value': 'Reset' }
-]
+    orders = get_orders_to_display()
+    queue_count = get_queued_count()
+    recently_placed_count = get_recently_placed_count()
+    recently_processed_count = get_recently_processed_count()
+    scenarios = [
+        { 'display': 'Add broken order', 'value': 'BrokenOrder' },
+        { 'display': 'Monitoring Load', 'value': 'HighLoad' },
+        { 'display': 'Queue Reliability', 'value': 'UnreliableProcessing' },
+        { 'display': 'System Monitoring', 'value': 'VeryHighLoad' },
+        { 'display': 'Reset to initial', 'value': 'Reset' }
+    ]
 
-return render_template(
-"layout.html", orders=orders, queue_count=queue_count, recently_placed_count=recently_placed_count,
-recently_processed_count=recently_processed_count, scenarios=scenarios
-)
+    return render_template(
+        "layout.html", orders=orders, queue_count=queue_count, recently_placed_count=recently_placed_count,
+        recently_processed_count=recently_processed_count, scenarios=scenarios
+    )
 
 @app.route("/count")
 def count():
-return { 'count': count_orders() }
+    return { 'count': count_orders() }
 
 
 @app.route("/new", methods=["POST"])
 def new_order():
-product = request.json["product"]
-customer = request.json["customer"]
-date_placed = request.json["date_placed"] or datetime.now(tz=timezone.utc)
-download = create_product_download(product)
-try:
-order = add_order(product, customer, date_placed, None, download)
-except Exception as e:
-return str(e)
+    product = request.json["product"]
+    customer = request.json["customer"]
+    date_placed = request.json["date_placed"] or datetime.now(tz=timezone.utc)
+    download = create_product_download(product)
+    try:
+        order = add_order(product, customer, date_placed, None, download)
+    except Exception as e:
+        return str(e)
 
-return f"Added: {order}"
+    return f"Added: {order}"
 
 
 @app.route("/scenario", methods=["POST"])
 def set_scenario():
-scenario = request.form["scenario"]
+    scenario = request.form["scenario"]
 
-if scenario == 'BrokenOrder':
-product = 'Product from the future'
-download = create_product_download(product)
-add_order('Product from the future', 'Me', '3000-01-01T12:00:00Z', None, download)
-return redirect('/')
+    if scenario == 'BrokenOrder':
+        product = 'Product from the future'
+        download = create_product_download(product)
+        add_order('Product from the future', 'Me', '3000-01-01T12:00:00Z', None, download)
+        return redirect('/')
 
-if scenario == 'Reset':
-clear_orders()
+    if scenario == 'Reset':
+        clear_orders()
 
-response = requests.post(
-app.config["FINANCE_PACKAGE_URL"] + "/scenario",
-json=scenario
-)
+    response = requests.post(
+        app.config["FINANCE_PACKAGE_URL"] + "/scenario",
+        json=scenario
+    )
+    response.raise_for_status()
 
-response.raise_for_status()
-
-return redirect('/')
+    return redirect('/')
 
 if __name__ == "__main__":
-app.run()
+    app.run()
